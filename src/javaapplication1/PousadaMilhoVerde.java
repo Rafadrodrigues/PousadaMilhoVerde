83bada0037b292faa3461d0e4c4d7792ee0a840f
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2023-07-19 12:23:50 -0300

    comecei a passar a limpo

--- src/javaapplication1/PousadaMilhoVerde.java
+++ src/javaapplication1/PousadaMilhoVerde.java
@@ -14,7 +14,7 @@ public class PousadaMilhoVerde {
 
     public static void main(String[] args) throws IOException, ParseException {
         Sistema sistema= new Sistema();
-        sistema.criarAdministrador("getulio", "123456", "getulio santos", "14612312399", "diamantina", "389980385", "getulio.santos@email", 5000);
+       
         //QUESTÃO 1
         //IMPLEMENTAMOS TODOS AS CLASSES E METODOS QUE PLANEJAVAMOS
         //REMODELAMOS DE ACORDO COM AS ALTERAÇÕES
@@ -23,6 +23,12 @@ public class PousadaMilhoVerde {
         //QUESTÃO 2
         //1- CRIAMOS NOSSSAS CLASSES DE ACORDO COM O LATEX MAS NOVAS ALTERAÇÕES FORAM NECESSÁRIAS
         //2- POR MEIO DO MÉTODO LOGIN APENAS OS FUNCIONARIOS CADASTRADOS PODEM ACESSAR O SISTEMA
+        sistema.FazerLogin("getulio", "123456");
+        //3-TODOS OS METODOS TO STRING ESTÃO SOBRECARREGADO
+        Cliente cliente = new Cliente("Rafa","12345678900","Diamantina","3198198182","rafa@email");
+        cliente.toString();
+        //4- AS CLASSES QUE HERDÃO JÁ ESTÃO COM O SUPER IMPLANTADOS
+         sistema.criarAdministrador("getulio", "123456", "getulio santos", "14612312399", "diamantina", "389980385", "getulio.santos@email", 5000);
         //QUESTÃO 3
         //
     }
